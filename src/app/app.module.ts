6396bddfaefd6bbf513a73f2e14b8f87b56ce21b
MellitiYassine <yassine.melliti@etudiant-fst.utm.tn>
2024-10-20 21:26:24 +0100

    feat: integrate PrimeNG library, add assets, and disable standalone mode

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
